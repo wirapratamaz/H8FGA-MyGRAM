037c23e0a3d7abe720af4cf7ec9fb71fb5d0a32c
wirapratamaz <wira@gringgo.co>
2023-04-15 09:52:15 +0800

    add helper response and impement middleware

---
+++ app/response/responseHelper.go
@@ -0,0 +1,32 @@
+package response
+
+import (
+	"net/http"
+
+	"github.com/gin-gonic/gin"
+)
+
+func BadRequestResponse(ctx *gin.Context, payload interface{}) {
+	WriteJsonResponse(ctx, http.StatusBadRequest, gin.H{
+		"error":   true,
+		"message": payload,
+	})
+}
+
+func InternalServerJsonResponse(ctx *gin.Context, payload interface{}) {
+	WriteJsonResponse(ctx, http.StatusInternalServerError, gin.H{
+		"error":   true,
+		"message": payload,
+	})
+}
+
+func NotFoundResponse(ctx *gin.Context, payload interface{}) {
+	WriteJsonResponse(ctx, http.StatusNotFound, gin.H{
+		"error":   true,
+		"message": payload,
+	})
+}
+
+func WriteJsonResponse(ctx *gin.Context, status int, payload interface{}) {
+	ctx.JSON(status, payload)
+}
