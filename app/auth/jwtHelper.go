df1d785de58aeb85644d6211ffb7d29e61c7d129
wirapratamaz <wira@gringgo.co>
2023-04-15 09:49:15 +0800

    add helper hash password and jwt

---
+++ app/auth/jwtHelper.go
@@ -0,0 +1,40 @@
+package auth
+
+import (
+	"fmt"
+
+	"github.com/golang-jwt/jwt/v4"
+)
+
+var secretKey = "rahasia"
+
+func GenerateToken(id uint, email string) string {
+	claims := jwt.MapClaims{
+		"id":    id,
+		"email": email,
+	}
+	parseToken := jwt.NewWithClaims(jwt.SigningMethodHS256, claims)
+	signedToken, _ := parseToken.SignedString([]byte(secretKey))
+
+	return signedToken
+}
+
+func VerifyToken(tokenString string) (interface{}, error) {
+	token, err := jwt.Parse(tokenString, func(t *jwt.Token) (interface{}, error) {
+		if _, ok := t.Method.(*jwt.SigningMethodHMAC); !ok {
+			return nil, fmt.Errorf("unauthorized")
+		}
+
+		return []byte(secretKey), nil
+	})
+
+	if err != nil {
+		return nil, err
+	}
+
+	if _, ok := token.Claims.(jwt.MapClaims); !ok && !token.Valid {
+		return nil, fmt.Errorf("unauthorized")
+	}
+
+	return token.Claims.(jwt.MapClaims), nil
+}
--- go.mod
+++ go.mod
@@ -13,6 +13,7 @@ require (
 	github.com/go-playground/universal-translator v0.18.1 // indirect
 	github.com/go-playground/validator/v10 v10.12.0 // indirect
 	github.com/goccy/go-json v0.10.0 // indirect
+	github.com/golang-jwt/jwt/v4 v4.5.0 // indirect
 	github.com/jackc/pgpassfile v1.0.0 // indirect
 	github.com/jackc/pgservicefile v0.0.0-20221227161230-091c0ba34f0a // indirect
 	github.com/jackc/pgx/v5 v5.3.0 // indirect
